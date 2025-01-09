2c95b0adb342345cd81f9b4d0cbcb5f77ba2190f
zortinon <86236370+ZoRiiii@users.noreply.github.com>
2025-01-09 16:42:56 +0700

    added funny comments

--- source/menu.h
+++ source/menu.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath> 
 
+// Global resources (textures)
 struct global_resources
 {
     static void initialize();
@@ -18,23 +19,32 @@ class Button : public sf::Drawable
     sf::Text button_text;
 
 public:
+    // Initialize of button
     Button(sf::Vector2f size, sf::Color color, sf::Vector2f position, sf::String text);
 
+    // Mouse on button?
     bool isMouseOver(const sf::Vector2i& mousePos) const;
 
+    // Draw it
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
+
+// Nukchix moment
 class Menu : public sf::Drawable
 {
+    // Button vector wtf
     std::vector<Button> buttons;
 
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
 public:
+    // Hype
     Menu();
     
+    // Logic? Noo...
     int button_events(sf::Event& event, sf::RenderWindow& window);
 
+    // Draw like a pro
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
--- source/world.hpp
+++ source/world.hpp
@@ -5,10 +5,12 @@
 #include <random>
 #include <cmath>
 
+// Super cool config
 #define WORLD_SIZE_W 75
 #define WORLD_SIZE_H 46
 #define TILE_SIZE 32
 
+// Tiles textures
 struct tile_textures
 {
     static sf::Texture dirt;
@@ -18,19 +20,28 @@ struct tile_textures
     static void initialize();
 };
 
+// World class! Sounds good!
 class World : public sf::Drawable
 {   
+    // Cringe moment 
     int world_map[WORLD_SIZE_H + 1][WORLD_SIZE_W + 1];
 
+    // WHoooa!!! TILES!!
     sf::RectangleShape tiles[WORLD_SIZE_H * WORLD_SIZE_W + 1];
+
+    // Optimization, or kostil?
     int total_tiles = 0;
 
+    // Degeneration :D
     void generate_test_flat_world();
     void generate_tile_render_map();
 public:
+    // Generating new world (super flat as default)
     World();
 
+    // Set postion of world. Test movement
     void set_position(int x, int y);
 
+    // Draw it like que pro
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
